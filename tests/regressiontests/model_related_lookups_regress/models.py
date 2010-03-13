fff77bf9143140a631c0ae33c0745a863bb6f5b3
Joel Watts <joel@joelwatts.com>
2010-03-12 18:09:25 -0600

    Got tests to break on M2M.

--- tests/regressiontests/model_related_lookups_regress/models.py
+++ tests/regressiontests/model_related_lookups_regress/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Season(models.Model):
     year = models.PositiveSmallIntegerField()
+    world_series_winner = models.CharField(max_length=100)
 
     def __unicode__(self):
         return unicode(self.year)
@@ -17,23 +18,64 @@ class Game(models.Model):
         return u"%s at %s" % (self.away, self.home)
 
 
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    games = models.ManyToManyField(Game, related_name='players')
+
+    def __unicode__(self):
+        return self.name
+
+
 __test__ = {'API_TESTS':"""
 Regression test for #11670
 
->>> season = Season.objects.create(year=2010)
->>> season
-<Season: 2010>
+>>> season_2009 = Season.objects.create(year=2009, world_series_winner="New York Yankees")
+>>> cardinals_at_astros = season_2009.games.create(home="Houston Astros", away="St. Louis Cardinals")
 
->>> Game.objects.create(season=season, home="Houston Astros", away="Chicago Cubs")
-<Game: Chicago Cubs at Houston Astros>
+>>> season_2010 = Season.objects.create(year=2010, world_series_winner="Houston Astros")
+>>> cubs_at_astros = season_2010.games.create(home="Houston Astros", away="Chicago Cubs")
+>>> brewers_at_astros = season_2010.games.create(home="Houston Astros", away="Milwaukee Brewers")
 
->>> Game.objects.all()
-[<Game: Chicago Cubs at Houston Astros>]
+>>> Game.objects.count()
+3
 
->>> Game.objects.filter(season__year=2010)
-[<Game: Chicago Cubs at Houston Astros>]
+>>> Game.objects.filter(season__year=2010).count()
+2
 
->>> Game.objects.get(season__year=2010)
-<Game: Chicago Cubs at Houston Astros>
+>>> Game.objects.get(season__year=2009)
+<Game: St. Louis Cardinals at Houston Astros>
+
+>>> hunter_pence = Player.objects.create(name="Hunter Pence")
+>>> hunter_pence.games = Game.objects.filter(season__year__in=[2009, 2010])
+
+>>> hunter_pence.games.count()
+3
+
+>>> hunter_pence.games.filter(season__year=2009).count()
+1
+
+>>> hunter_pence.games.filter(season__year=2010).count()
+2
+
+>>> pudge = Player.objects.create(name="Ivan Rodriquez")
+>>> pudge.games = Game.objects.filter(season__year=2009)
+
+>>> pedro_feliz = Player.objects.create(name="Pedro Feliz")
+>>> pedro_feliz.games = Game.objects.filter(season__year=2010)
+
+>>> Player.objects.count()
+3
+
+>>> Player.objects.filter(games__season__year=2009).distinct().count()
+2
+
+>>> Player.objects.filter(games__season__year=2010).distinct().count()
+2
+
+>>> Player.objects.filter(games__season__world_series_winner__icontains="Yankees").distinct().count()
+2
+
+>>> Player.objects.filter(games__season__world_series_winner__iexact="Houston Astros").distinct().count()
+2
 
 """}
